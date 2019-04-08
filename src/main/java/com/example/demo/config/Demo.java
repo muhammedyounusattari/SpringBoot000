d6de0ec9dc2dd2a8f2498fd3f302685fbf003cac
Younus <muhammedyounusattari@gmail.com>
2019-04-08 11:43:20 +0530

    Moving json file to root location

--- src/main/java/com/example/demo/config/Demo.java
+++ src/main/java/com/example/demo/config/Demo.java
@@ -10,6 +10,9 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.example.demo.model.Automation;
 import com.example.demo.model.Commands;
 import com.fasterxml.jackson.core.JsonGenerationException;
@@ -21,30 +24,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Demo {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Demo.class);
+
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 
-		String path = "C:\\\\Users\\\\141418\\\\hitachi-workspace\\\\SpringBoot00\\\\src\\\\main\\\\resources\\\\test.json";
+		String path = "test.json";
 
 		ObjectMapper mapper = new ObjectMapper();
 		Automation automation = new Automation();
 
-		try(PrintStream ps = new PrintStream(new FileOutputStream(new File("result.properties")));) {
-		automation = mapper.readValue(new File(path), Automation.class);
-		List<Commands> commands = automation.getTests().get(0).getCommands();
-		Iterator<Commands> itr = commands.listIterator();
-		while(itr.hasNext()) {
-			Commands command = itr.next();
-			if(command.getTargets().size()>0) {
-				String s = command.getCommand()+"="+command.getTargets().get(3)+"\n";
-				ps.print(s);
-				System.out.println("please check the output generated file....");
+		try (PrintStream ps = new PrintStream(new FileOutputStream(new File("result.properties")));) {
+			automation = mapper.readValue(new File(path), Automation.class);
+			List<Commands> commands = automation.getTests().get(0).getCommands();
+			Iterator<Commands> itr = commands.listIterator();
+			while (itr.hasNext()) {
+				Commands command = itr.next();
+				if (command.getTargets().size() >= 4) {
+					ps.print(command.getCommand() + "=" + command.getTargets().get(3) + "\n");
+
+				} else if (command.getTargets().size() > 0) {
+					ps.print(command.getCommand() + "=" + command.getTargets().get(1) + "\n");
+				}
 			}
-			}
-		}catch(IOException e) {
+			LOG.info("please check the output generated file....");
+		} catch (IOException e) {
 			e.printStackTrace();
+			LOG.error("Pipe is broken some where " + e.getMessage());
 		}
-		
-		
+
 		/*
 		 * try { automation = mapper.readValue(new File(path), Automation.class);
 		 * List<Commands> commands = automation.getTests().get(0).getCommands();
